327fee42489cc590fe210ed24a5dcc03e388cb56
milinches <ibrogenius5165@gmail.com>
2021-09-13 11:30:45 +0100

    fixed changes *made

--- models/models.go
+++ models/models.go
@@ -11,6 +11,12 @@ func Uni() []*Universities {
 		&Universities{Name: "Abia State University, Uturu", Abbrv: "ABSU", WebsiteLink: "https://abiastateuniversity.edu.ng/"},
 		&Universities{Name: "Achievers University, Owo", Abbrv: "AUO", WebsiteLink: "https://www.achievers.edu.ng/"},
 		&Universities{Name: "Adamawa State University", Abbrv: "ADSU", WebsiteLink: "https://adsu.edu.ng/"},
+		&Universities{Name: "Adekunle Ajasin University", Abbrv: "AAUA", WebsiteLink: "https://aaua.edu.ng/"},
+		&Universities{Name: "Adeleke University, Ede", Abbrv: "AUE", WebsiteLink: "https://adelekeuniversity.edu.ng/"},
+		&Universities{Name: "Admiralty University of Nigeria", Abbrv: "ADUN", WebsiteLink: "https://adun.edu.ng/"},
+		&Universities{Name: "Afe Babalola University", Abbrv: "ABUAD", WebsiteLink: "https://www.abuad.edu.ng/"},
+		&Universities{Name: "African Univeristy of Science and Technology", Abbrv: "AUST", WebsiteLink: "https://aust.edu.ng/"},
+		&Universities{Name: "", Abbrv: "", WebsiteLink: ""},
 		&Universities{Name: "", Abbrv: "", WebsiteLink: ""},
 	}
 }
