8a85e0a417eb100d40c439fc8fee59a958858267
milinches <ibrogenius5165@gmail.com>
2021-09-18 20:17:47 +0100

    Added all uni and abbrv.

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -26,5 +26,5 @@ func GetSpecificUniversity(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-	w.Write([]byte("Sorry, this is not Available"))
+	w.Write([]byte("Sorry, endpoint not available 😴"))
 }
