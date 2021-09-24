60a7e87889ad80318c1a00741c8350b23cd10917
milinches <ibrogenius5165@gmail.com>
2021-09-24 12:05:18 +0100

    Added .env file

--- main.go
+++ main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/milinches/nigeria-universities-api/routes"
 )
 
 func main() {
+	envErr := godotenv.Load(".env")
+	if envErr != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port, exist := os.LookupEnv("PORT")
+	if !exist {
+		log.Fatal("No .env file found")
+	}
 	router := routes.NewRouter()
-	log.Println("Starting server at :8080")
-	err := http.ListenAndServe(":8080", router)
+	log.Println("Starting server at :8000")
+	err := http.ListenAndServe(":"+port, router)
 	log.Fatal(err)
 }
